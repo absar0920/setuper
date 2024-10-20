197a86c47f35dc298d45ca4983d7a349f337122c
absar <absar0920@proton.me>
2024-10-20 16:11:27 +0500

    [update] aws region setup

--- scripts/installer/my/script_deb.sh
+++ scripts/installer/my/script_deb.sh
@@ -43,6 +43,7 @@ sudo snap install aws-cli --classic
 if [ -f .env ]; then
     aws --profile default configure set aws_access_key_id $AWS_ACCESS_KEY_ID
     aws --profile default configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+    aws --profile default configure set region $AWS_REGION
     mv ~/.aws/ /home/$MACHINE_USER/
     sudo chown -R $MACHINE_USER:$MACHINE_USER /home/$MACHINE_USER/.aws
     sudo chmod 600 /home/$MACHINE_USER/.aws/credentials /home/$MACHINE_USER/.aws/config
