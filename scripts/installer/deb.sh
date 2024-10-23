7d4bc4de05662370a0689f67e6635dd978f3ad80
absar <absar0920@proton.me>
2024-10-23 18:24:06 +0500

    [update] add browsers

--- scripts/installer/deb.sh
+++ scripts/installer/deb.sh
@@ -51,11 +51,22 @@ if [ "$MINE" == "true" ]; then
     sudo DEBIAN_FRONTEND=noninteractive apt-get install wireshark -y
     sudo apt-get install curl -y
     sudo apt-get install speedtest-cli alsamixergui -y
-    sudo apt-get install rustc cargo -y
+    sudo apt-get install rustc cargo golang -y
     sudo snap install obsidian --classic
     sudo apt-get install obs-studio -y
 
     sudo apt-get install aria2 -y
+
+    # install thorium browser, opera, vivaldi, mercury
+    wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
+    sudo mv thorium.list /etc/apt/sources.list.d/
+    sudo apt update -y
+    sudo apt install thorium-browser -y
+    sudo sh -c 'echo "deb [arch=amd64] https://deb.opera.com/opera-stable/ stable non-free" > /etc/apt/sources.list.d/opera.list'
+    wget -O - https://deb.opera.com/archive.key | sudo apt-key add -
+    sudo apt-get update -y
+    sudo apt-get install opera-stable -y
+    sudo snap install vivaldi
 
     bash scripts/installer/my/script_deb.sh
 fi
