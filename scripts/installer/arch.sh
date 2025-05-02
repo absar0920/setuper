fc40f40b5712c2a4e63a3b68202396af9a44514c
absar0920 <absar0920@proton.me>
2025-05-02 11:00:45 +0500

    fixed yay sudo issue

--- scripts/installer/arch.sh
+++ scripts/installer/arch.sh
@@ -6,7 +6,7 @@ source ./setup_utils.sh "$@"
 
 if [ "$REACT_NATIVE" == "true" -o "$OPTIONS" == "all" ]; then
     sudo pacman -S --noconfirm jdk17-openjdk jre17-openjdk
-    yay -S --noconfirm android-studio
+    sudo -u $MACHINE_USER yay -S --noconfirm android-studio
     sudo npm install -g react-native-cli
 fi
 
@@ -32,12 +32,12 @@ fi
 
 if [ "$POSTMAN" == "true" -o "$OPTIONS" == "all" -o "$MINE" == "true" -o "$RECOMMENDED" == "true" ]; then
     echo "INSTALLING POSTMAN..."
-    yay -S --noconfirm postman-bin
+    sudo -u $MACHINE_USER yay -S --noconfirm postman-bin
 fi
 
 if [ "$MINE" == "true" -o "$WHATSAPP" == "true" -o "$RECOMMENDED" == "true" -o "$OPTIONS" == "all" ]; then
     echo "INSTALLING WHATSAPP..."
-    yay -S --noconfirm whatsie whatsdesk-bin
+    sudo -u $MACHINE_USER yay -S --noconfirm whatsie whatsdesk-bin
 fi
 
 if [ "$TOR" == "true" -o "$MINE" == "true" ]; then
@@ -58,8 +58,8 @@ if [ "$MINE" == "true" ]; then
     sudo pacman -S --noconfirm aria2
 
     # Install Thorium Browser, Opera, Vivaldi, Mercury
-    yay -S --noconfirm thorium-browser-bin
-    yay -S --noconfirm opera
+    sudo -u $MACHINE_USER yay -S --noconfirm thorium-browser-bin
+    sudo -u $MACHINE_USER yay -S --noconfirm opera
     sudo pacman -S --noconfirm vivaldi
 
     bash scripts/installer/my/script_arch.sh
